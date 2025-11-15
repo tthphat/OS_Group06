466d4b2d71b9db274a966762f07b4c82e12e78b8
BuuHuynhVinhPhuc <buuhuynhvinhphuc@gmail.com>
2025-11-15 08:28:31 -0500

    Small update to find.c

--- user/find.c
+++ user/find.c
@@ -4,25 +4,6 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
-char*
-fmtname(char *path)
-{
-  static char buf[DIRSIZ+1];
-  char *p;
-
-  // Find first character after last slash.
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-    ;
-  p++;
-
-  // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
-    return p;
-  memmove(buf, p, strlen(p)); // buff = p (deep copy)
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p)); 
-  return buf;
-}
-
 void
 find(char* path, char* name) {
     char buf[512], *p;
