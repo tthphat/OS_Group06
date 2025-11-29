28b789c643c93b71a80269baf6f506e38221b4e1
Truong Thanh Phat <truongthanhphat1103@gmail.com>
2025-11-29 21:23:03 +0700

    chore(sysinfo): re-write clearly commentary of load avg function.

--- kernel/proc.c
+++ kernel/proc.c
@@ -716,7 +716,7 @@ static uint64 loadavg = 0;
 static int loadavg_samples = 0;
 
 uint64 compute_loadavg(void) {
-    int current_nproc = nproc();   // lấy số tiến trình hiện tại
+    int current_nproc = nproc();   // get the number of currently active processes
     loadavg = (loadavg * loadavg_samples + current_nproc) / (loadavg_samples + 1);
     loadavg_samples++;
     
