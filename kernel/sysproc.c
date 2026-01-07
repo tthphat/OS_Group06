a937a30085aba0621a8b86d2ab95946f4711c777
Truong Thanh Phat <truongthanhphat1103@gmail.com>
2026-01-07 23:04:03 +0700

    feat(detect): implement sys_pgaccess in sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -123,3 +123,43 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+    uint64 start_va;
+    int npages;
+    uint64 user_mask;
+
+    if (argaddr(0, &start_va) < 0 ||
+        argint(1, &npages) < 0 ||
+        argaddr(2, &user_mask) < 0)
+        return -1;
+
+    if (npages > 64)
+        npages = 64;
+
+    uint64 mask = 0;
+    struct proc *p = myproc();
+
+    for (int i = 0; i < npages; i++) {
+        uint64 va = start_va + i * PGSIZE;
+
+        pte_t *pte = walk(p->pagetable, va, 0);
+        if (pte == 0)
+            continue;
+
+        if (*pte & PTE_A) {
+            mask |= (1L << i);
+            *pte &= ~PTE_A;
+        }
+    }
+
+    if (copyout(p->pagetable, user_mask, (char *)&mask, sizeof(mask)) < 0)
+        return -1;
+
+    return 0;
+}
+#endif
