4e326c4a6736793a890bd1772ad3dda3e0e8ce62
NguyenKhaNhuHCMUS <23120311@student.hcmus.edu.vn>
2026-01-08 09:12:28 +0700

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -79,6 +79,10 @@ struct trapframe {
   /* 280 */ uint64 t6;
 };
 
+struct usyscall {
+  int pid;
+};
+
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
@@ -100,6 +104,7 @@ struct proc {
   uint64 sz;                   // Size of process memory (bytes)
   pagetable_t pagetable;       // User page table
   struct trapframe *trapframe; // data page for trampoline.S
+  struct usyscall *usyscall;
   struct context context;      // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
